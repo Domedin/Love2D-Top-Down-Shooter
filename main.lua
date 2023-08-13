bde02849ac93c210e5f91c295e20ebb15edb8294
Domedin <jackanderson080307@gmail.com>
2023-08-13 13:24:25 -0400

    Sprites setup

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 function love.load()
-
+    Player = {}
+    Player.x = love.graphics.getWidth() / 2
+    Player.y = love.graphics.getHeight() / 2
+    
+    Sprites = {}
+    Sprites.idlePlayer = love.graphics.newImage('sprites/IdlePlayer.png')
+    Sprites.shootingPlayer = love.graphics.newImage('sprites/IdlePlayer.png')
+    Sprites.background = love.graphics.newImage('sprites/Background.png')
+    Sprites.bullet = love.graphics.newImage('sprites/Bullet.png')
+    Sprites.enemy = love.graphics.newImage('sprites/Enemy.png')
 end
 
 function love.update(dt)
@@ -7,5 +16,7 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    love.graphics.draw(Sprites.background, 0, 0)
+    
+    love.graphics.draw(Sprites.idlePlayer, Player.x, Player.y)
 end
