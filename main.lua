1be85a36186faa8018ba1698b6bcefd4c4e39a76
Domedin <jackanderson080307@gmail.com>
2023-08-13 13:37:37 -0400

    Functional movement needs work

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ function love.load()
     Player = {}
     Player.x = love.graphics.getWidth() / 2
     Player.y = love.graphics.getHeight() / 2
+    Player.speed = 180
     
     Sprites = {}
     Sprites.idlePlayer = love.graphics.newImage('sprites/IdlePlayer.png')
@@ -12,11 +13,22 @@ function love.load()
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("w") then
+        Player.y = Player.y - Player.speed * dt
+    end
+    if love.keyboard.isDown("a") then
+        Player.x = Player.x - Player.speed * dt
+    end
+    if love.keyboard.isDown("s") then
+        Player.y = Player.y + Player.speed * dt
+    end
+    if love.keyboard.isDown("d") then
+        Player.x = Player.x + Player.speed * dt
+    end
 end
 
 function love.draw()
     love.graphics.draw(Sprites.background, 0, 0)
-    
+
     love.graphics.draw(Sprites.idlePlayer, Player.x, Player.y)
 end
